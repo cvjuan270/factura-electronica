53f62b32e91051b91c0eb7b1a8b79e71bf64b7af
unknown <cvjuan270@gmail.com>
2018-05-10 12:34:49 -0500

    Generacion de txt de factura terminado

--- APP/Dv_Inversiones/Dv_Inversiones/MainWindow.xaml.cs
+++ APP/Dv_Inversiones/Dv_Inversiones/MainWindow.xaml.cs
@@ -45,24 +45,78 @@ namespace Dv_Inversiones
 
         private void btnBeneratxt_Click(object sender, RoutedEventArgs e)
         {
+            ////Cabecera de factura//
             try
             {
                 string cmd = string.Format("EXEC	 [dbo].[ConsultaFT] @DOCNUM = {0}", txtIDFactura.Text.Trim());
                 DataSet ds = Utilidades.ejecutar(cmd);
+                TextWriter cabecera = new StreamWriter ("C:\\Users/Juan/Desktop/FT ELECTRONICA/Sample.txt");
 
-                    FileStream stream = new FileStream("C:\\Users/Juan/Desktop/FT ELECTRONICA/Sample.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                /*FileStream stream = new FileStream("C:\\Users/Juan/Desktop/FT ELECTRONICA/Sample.txt", FileMode.OpenOrCreate, FileAccess.Write);
                     StreamWriter writer = new StreamWriter(stream);
+                */
 
                 int Trows = ds.Tables[0].Rows.Count;
                     for (int f = 0;f<Trows;f++)
                     {
-                        writer.WriteLine(ds.Tables[0].Rows[f].ItemArray[0].ToString()+"|"+ds.Tables[1].Rows[0].ItemArray[f].ToString()+"|");
+                        cabecera.WriteLine(ds.Tables[0].Rows[f].ItemArray[0].ToString()+"|"+ds.Tables[1].Rows[0].ItemArray[f].ToString()+"|");
                     }
-                    writer.Close();
-                
+                    cabecera.Close();
+            }
+            catch (Exception ex) { MessageBox.Show("Error en Escritura de cabecera de factura" + ex); }
+
+            //Lineas de factura//
+            
+            try
+            {
+                string cmd = string.Format("EXEC	[dbo].[ConsultaFT_Lineas] @DocNum={0}",txtIDFactura.Text.Trim());
+                DataSet ds = Utilidades.ejecutar(cmd);
+                TextWriter lineasfactu = File.AppendText("C:\\Users/Juan/Desktop/FT ELECTRONICA/Sample.txt");
+                int trows = ds.Tables[0].Rows.Count;
+                int tcolu = ds.Tables[0].Columns.Count;
+                for (int i = 0; i < trows; i++)
+                {
+                    
+                    for (int j = 0; j < tcolu; j++)
+                    {
+                        lineasfactu.Write(ds.Tables[0].Rows[i].ItemArray[j].ToString()+"|");
+                    }
+                    lineasfactu.WriteLine();
+                }
+                lineasfactu.Close();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error en escribir las lineas de la factura");
+            }
+            //Guias de remision//
+
+            try
+            {
+                string cmd = string.Format("EXEC	 [dbo].[ConsultaFT_Guias] @DocNum = {0}",txtIDFactura.Text.Trim());
+                DataSet ds = Utilidades.ejecutar(cmd);
+
+                TextWriter guias = File.AppendText("C:\\Users/Juan/Desktop/FT ELECTRONICA/Sample.txt");
+                int trows = ds.Tables[0].Rows.Count;
+                int tinde = ds.Tables[0].Columns.Count;
+                for (int i = 0; i < trows; i++)
+                {
+                    for (int j = 0; j < tinde; j++)
+                    {
+                        guias.Write(ds.Tables[0].Rows[i].ItemArray[j].ToString());
+                    }
+                    guias.WriteLine();
+                }
+                guias.Close();
 
             }
-            catch (Exception ex) { MessageBox.Show("Error" + ex); }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Error al escribir guias de remision");
+            }
+            
         }
     }
 }
